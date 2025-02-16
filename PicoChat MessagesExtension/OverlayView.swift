2f1cc765e1c5b7ed48647dba7c7278c5108aae25
Idrees Hassan <idrees@idreesinc.com>
2025-02-15 19:12:10 -0500

    Disable stylus on iPad

--- PicoChat MessagesExtension/OverlayView.swift
+++ PicoChat MessagesExtension/OverlayView.swift
@@ -12,23 +12,25 @@ struct OverlayView: View {
     @Binding var touchX: CGFloat
     @Binding var touchY: CGFloat
     @Binding var touching: Bool
+    @Binding var stylusOrientation: StylusOrientation
     
     var body: some View {
-        ZStack {
-            let x = touching ? touchX + 350/2 : UIScreen.main.nativeBounds.width
-            let y = touching ? touchY + 350/2 : UIScreen.main.nativeBounds.width
-            Image("stylus")
-                .resizable()
-                .frame(width: 350, height: 350)
-                .zIndex(2)
-                .position(x: x, y: y)
-                .opacity(0.7)
-                .animation(.smooth(duration: touching ? 0.05 : 0.3), value: x)
-                .animation(.smooth(duration: touching ? 0.05 : 0.3), value: y)
+        if stylusOrientation != .disabled {
+            ZStack {
+                let x = touching ? touchX + 350/2 : UIScreen.main.nativeBounds.width
+                let y = touching ? touchY + 350/2 : UIScreen.main.nativeBounds.width
+                Image("stylus")
+                    .resizable()
+                    .frame(width: 350, height: 350)
+                    .zIndex(2)
+                    .position(x: x, y: y)
+                    .opacity(0.7)
+                    .animation(.smooth(duration: touching ? 0.05 : 0.3), value: x)
+                    .animation(.smooth(duration: touching ? 0.05 : 0.3), value: y)
+            }
+            .edgesIgnoringSafeArea(.all)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .allowsHitTesting(false)
         }
-        .edgesIgnoringSafeArea(.all)
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-//        .background(Color.black.opacity(0.5))
-        .allowsHitTesting(false)
     }
 }
--- PicoChat MessagesExtension/ParentView.swift
+++ PicoChat MessagesExtension/ParentView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 import Messages
 
+enum StylusOrientation {
+    case right
+    case left
+    case disabled
+}
+    
+
 struct ParentView: View {
     
     @ObservedObject var presentationStyleWrapper: PresentationStyleWrapper
     @ObservedObject var conversationWrapper: ConversationWrapper
     
+    @State var stylusOrientation = StylusOrientation.right
     @State var touching: Bool = false
     @State var touchX: CGFloat = 0
     @State var touchY: CGFloat = 0
@@ -20,25 +28,33 @@ struct ParentView: View {
     
     var body: some View {
         ZStack {
-            OverlayView(touchX: $touchX, touchY: $touchY, touching: $touching)
+            OverlayView(touchX: $touchX, touchY: $touchY, touching: $touching, stylusOrientation: $stylusOrientation)
                 .zIndex(20)
             PicoChatView(presentationStyleWrapper: presentationStyleWrapper, conversationWrapper: conversationWrapper, touching: $touching)
-                .simultaneousGesture(
-                    DragGesture(minimumDistance: 0, coordinateSpace: .named("screen"))
-                        .onChanged { value in
-                            if Date().timeIntervalSince(timeOfLastUpdate) < 1.0 / 60.0 {
-                                return
+                .applyIf(stylusOrientation != StylusOrientation.disabled) { view in
+                    view.simultaneousGesture(
+                        DragGesture(minimumDistance: 0, coordinateSpace: .named("screen"))
+                            .onChanged { value in
+                                if Date().timeIntervalSince(timeOfLastUpdate) < 1.0 / 60.0 {
+                                    return
+                                }
+                                timeOfLastUpdate = Date()
+                                touchX = value.location.x
+                                touchY = value.location.y
                             }
-                            timeOfLastUpdate = Date()
-                            touchX = value.location.x
-                            touchY = value.location.y
-                        }
-                        .onEnded { _ in
-                        }
-                )
+                            .onEnded { _ in
+                            }
+                    )
+                }
         }
         .edgesIgnoringSafeArea(.all)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onAppear {
+            // Check if the device is an iPad
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                stylusOrientation = .disabled
+            }
+        }
     }
 }
 
